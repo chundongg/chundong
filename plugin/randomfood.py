c860b25f364d88f54946801d9b3b2ccc17eb8bd3
C c <53864741+chundongg@users.noreply.github.com>
2022-02-06 18:57:37 +0800

    Create randomfood.py

---
+++ plugin/randomfood.py
@@ -0,0 +1,5 @@
+import random
+
+def randomfood() -> str:
+    result = random.choice(["豆浆记忆","一楼打菜","香锅","饺子","重庆面","黄焖鸡","早餐店左","烤盘","淮南牛肉面","石锅","拉面","烤肉拌饭","清食","水果捞","二楼打菜","汉堡","烤鸭","麻辣烫"])
+    return result
