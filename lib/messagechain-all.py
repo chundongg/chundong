8beb6b6a7be1209f33eb3fc19baff34682993d5c
C c <53864741+chundongg@users.noreply.github.com>
2022-02-06 14:54:48 +0800

    Create messagechain-all.py

---
+++ lib/messagechain-all.py
@@ -0,0 +1,67 @@
+from pydoc import plain
+from graia.application.event.messages import FriendMessage, GroupMessage,TempMessage
+from graia.application import BotMessage, Friend, GraiaMiraiApplication, Group, Member
+from graia.broadcast import Broadcast, DispatcherInterface
+from graia.application.message.chain import MessageChain
+from graia.application.message.elements.internal import Image, Plain
+
+from .readfile import readfile
+
+
+def allmessagechain(bcc: Broadcast):
+    @bcc.receiver(GroupMessage)
+    async def sendmessage(event:GroupMessage,user:Member,group:Group):
+        info = await readfile('../setting/premssion.yaml')
+        if user.id in info['admin']['id']:
+            level = info['admin']['level']
+        elif user.id in info['banner']['id']:
+            level = info['banner']['level']
+        else:
+            level = info['user']['leve']
+        request = 1
+        result = 1
+        async def eachsendmessage(
+            app: GraiaMiraiApplication,
+            message: MessageChain,
+            group: Group,
+            user: Member,
+            level=level,
+            request=request,
+            result=result
+        ):
+            if level >= 10:
+                if message.asDisplay().startswith(request):
+                    try:
+                        await app.sendGroupMessage(group,MessageChain.create([
+                            Plain("{},{}".format(user.id,result))
+                        ]))
+                    except:
+                        await app.sendGroupMessage()
+    @bcc.receiver(FriendMessage)
+    async def sendmessage(event:FriendMessage,user:Friend):
+        info = await readfile('../setting/premssion.yaml')
+        if user.id in info['admin']['id']:
+            level = info['admin']['level']
+        elif user.id in info['banner']['id']:
+            level = info['banner']['level']
+        else:
+            level = info['user']['leve']
+        bcc.postEvent(
+            FriendMessage(
+
+            )
+        )
+    @bcc.receiver(TempMessage)
+    async def sendmessage(event:TempMessage,user:Member):
+        info = await readfile('../setting/premssion.yaml')
+        if user.id in info['admin']['id']:
+            level = info['admin']['level']
+        elif user.id in info['banner']['id']:
+            level = info['banner']['level']
+        else:
+            level = info['user']['leve']
+        bcc.postEvent(
+            TempMessage(
+
+            )
+        )
