4ec6a6176fce3c515d0ed6c357ebb17709fc9e82
C c <53864741+chundongg@users.noreply.github.com>
2022-02-07 12:53:47 +0800

    Update premssion.py

--- lib/premssion.py
+++ lib/premssion.py
@@ -1,6 +1,7 @@
 from .readfile import readfile
+import yaml
 
-def confirmpremssion(id):
+def confirmpremssion(id) -> int:
     info = readfile('./setting/premssion.yaml')
     id = str(id)
     if id in info['admin']['id']:
@@ -10,3 +11,16 @@ def confirmpremssion(id):
     else:
         level = info['user']['level']
     return level
+
+def delpremssion(id,group) -> bool:
+    try:
+        info = readfile('./setting/premssion.yaml')
+        grouplist = ['admin','user','banner']
+        grouplist.remove(group)
+        for i in grouplist:
+            if id in info[i]['id']:
+                info[i]['id'].remove(id)
+        yaml.dump(info,open('./setting/premssion.yaml','w',encoding='utf-8'))
+        return True
+    except:
+        return False
