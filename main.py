f09d0d7e604091dc55eac68d050cc632b6516b03
C c <53864741+chundongg@users.noreply.github.com>
2022-02-07 12:53:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import asyncio
+from pydoc import plain
+import re
 from graia.broadcast import Broadcast
 from graia.application import GraiaMiraiApplication, Session
 from graia.application import GraiaMiraiApplication, Group, Member
@@ -6,7 +8,9 @@ from graia.application.message.chain import MessageChain
 from graia.application.message.elements.internal import Plain,Image
 
 from lib.readfile import readfile
+from lib.readfile import upgradefile
 from lib.premssion import confirmpremssion
+from lib.premssion import delpremssion
 from lib.time import localtime
 
 from plugin.randomfood import randomfood
@@ -48,13 +52,60 @@ if __name__ == '__main__':
                 if confirmpremssion(user.id) >= 10:
                     try:
                         await app.sendGroupMessage(group,message.create([
-                            Plain("{},{}\n指令集:\n╠#随机吃饭 - 我也不知道吃啥啊！\n╠#帮我选择 - 啊对对对对！\n╠#二次元 - 杰哥注意上了你".format(data,user.name)
+                            Plain("{},{}\n指令集:\n╠#随机吃饭 - 我也不知道吃啥啊！\n╠#帮我选择 选择A 选择B ...\n╠#二次元 - 杰哥注意上了你".format(data,user.name)
                             )
                         ]))
                     except:
                         await app.sendGroupMessage(group,message.create([
                             Plain("按理来说，你看不到这个，这是因为发生了未知错误")
                         ]))
+    #setgroup 
+    @bcc.receiver("GroupMessage")
+    async def help(app: GraiaMiraiApplication, group: Group,user:Member,message:MessageChain):
+        if message.asDisplay().startswith('#setgroup '):
+            if confirmpremssion(user.id) >= 70:
+                word = re.sub("[^\w]", " ", message.asDisplay().replace("#setgroup ","")).split()
+                if len(word) == 2:
+                    try:
+                        wl = int(word[0])
+                    except:
+                        await app.sendGroupMessage(group,message.create([
+                            Plain("{},{}不是个有效的QQ号".format(user.name,word[0]))
+                        ]))
+                    else:
+                        if wl >= 10000000:
+                            if word[1] in ['admin','user','banner']:
+                                try:
+                                    if upgradefile('./setting/premssion.yaml',word[0],word[1]):
+                                        if delpremssion(word[0],word[1]):
+                                            await app.sendGroupMessage(group,message.create([
+                                                Plain("{},已经成功将用户{}加入权限组{}".format(user.name,word[0],word[1]))
+                                            ]))
+                                        else:
+                                            await app.sendGroupMessage(group,message.create([
+                                                Plain("#3按理来说，你看不到这个，这是因为发生了未知错误")
+                                            ]))
+                                    else:
+                                        await app.sendGroupMessage(group,message.create([
+                                            Plain("按理来说，你看不到这个，这是因为发生了未知错误")
+                                        ]))
+                                except:
+                                    await app.sendGroupMessage(group,message.create([
+                                        Plain("按理来说，你看不到这个，这是因为发生了未知错误")
+                                    ]))
+                            else:
+                                await app.sendGroupMessage(group,message.create([
+                                    Plain("{},权限组不存在\n权限组：[admin,user,banner]".format(user.name))
+                                ]))
+                        else:
+                            await app.sendGroupMessage(group,message.create([
+                                Plain("{},{}不是个有效的QQ号".format(user.name,word[0]))
+                            ]))
+                else:
+                    await app.sendGroupMessage(group,message.create([
+                        Plain("{},指令格式错误".format(user.name))
+                    ]))
+                
     #随机吃饭
     @bcc.receiver("GroupMessage")
     async def help(app: GraiaMiraiApplication, group: Group,user:Member,message:MessageChain):
