1c1883994986a2df4dbeff4b6aba1ee1b7fe423c
C c <53864741+chundongg@users.noreply.github.com>
2022-02-07 12:53:50 +0800

    Update readfile.py

--- lib/readfile.py
+++ lib/readfile.py
@@ -1,3 +1,4 @@
+from matplotlib.pyplot import close
 import yaml
 #读取配置文件,返回信息
 def readfile(local) ->dict: 
@@ -5,3 +6,14 @@ def readfile(local) ->dict:
         result = yaml.full_load(file)
     file.close()
     return result
+
+def upgradefile(local,id,group) -> bool:
+    try:
+        with open(local,'r',encoding = 'utf-8') as file:
+            result = yaml.full_load(file)
+        file.close()
+        result[group]['id'].append(id)
+        yaml.dump(result,open(local,'w',encoding='utf-8'))
+        return True
+    except:
+        return False
