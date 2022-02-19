a2038cde71790b02afb8c693a157a7239c32a300
C c <53864741+chundongg@users.noreply.github.com>
2022-02-19 14:00:00 +0800

    Update animepic.py
    新增pixiv

--- plugin/animepic.py
+++ plugin/animepic.py
@@ -3,9 +3,16 @@ from io import BytesIO
 from PIL import Image
 import uuid
 
-def get_animepic():
+def get_animepic() -> str:
     r = requests.get("http://iw233.cn/api/Random.php")
     image = Image.open(BytesIO(r.content))
     name = uuid.uuid4()
     image.save('./images/animepic/{}.jpg'.format(name))
+    return name
+
+def get_pixivpic() -> str:
+    r = requests.get("https://pximg2.rainchan.win/rawimg")
+    image = Image.open(BytesIO(r.content))
+    name = r.url[r.url.find("img_id=")+7:]
+    image.save('./images/pixivpic/{}.png'.format(name))
     return name
