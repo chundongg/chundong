997d0e66afffa03aa6ef0e276e1333dc370da735
chundongg <2265521774@qq.com>
2022-06-07 11:36:03 +0800

    Update hollworld.py
    Optimized requests and json

--- plugin/hollworld.py
+++ plugin/hollworld.py
@@ -1,7 +1,7 @@
 import requests
-import json
 
 def forworld() -> str:
     r = requests.get('https://api.iyk0.com/60s')
-    str_to_json = json.loads(r.text)
+    str_to_json = r.json()
+    r.close()
     return str_to_json['imageUrl']
