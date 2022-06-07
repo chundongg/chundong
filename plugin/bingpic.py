1b749ca49c5bbf4b258b79ce00811ff77aaad40b
chundongg <2265521774@qq.com>
2022-06-07 11:35:58 +0800

    Update bingpic.py
    Optimized requests and json

--- plugin/bingpic.py
+++ plugin/bingpic.py
@@ -1,12 +1,12 @@
 import requests
-import json
 import random
 from io import BytesIO
 from PIL import Image
 
 def get_bingpic() ->list:
     r = requests.get("https://api.iyk0.com/mryt/")
-    str_to_json = json.loads(r.text)
+    str_to_json = r.json()
+    r.close()
     pic = random.choice(str_to_json['data'])
     picname = pic['title']
     fixname = picname.replace('/','-')
@@ -14,4 +14,5 @@ def get_bingpic() ->list:
     picurl2 = requests.get(picurl)
     image = Image.open(BytesIO(picurl2.content))
     image.save('./images/bingpic/{}.jpg'.format(fixname))
+    picurl2.close()
     return pic
